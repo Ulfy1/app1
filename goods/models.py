0e08847448df7b772d74389e3a6fa8b70b7afb03
John Doe <johndoe@email.com>
2025-05-26 21:09:17 +0200

    Categories and Products associated with bd

--- goods/models.py
+++ goods/models.py
@@ -1,3 +1,29 @@
 from django.db import models
 
 # Create your models here.
+class Categories(models.Model):
+    name = models.CharField(max_length=150, unique=True, verbose_name='Category')
+    slug = models.SlugField(max_length=200, unique=True, blank=True, null=True, verbose_name='URL')
+    
+    def __str__(self):
+        return self.name
+
+    class Meta():
+        db_table = 'category'
+
+
+class Products(models.Model):
+    name = models.CharField(max_length=150, unique=True, verbose_name='Name')
+    slug = models.SlugField(max_length=200, unique=True, blank=True, null=True, verbose_name='URL')
+    description = models.TextField(blank=True, null=True, verbose_name='Description')
+    image = models.ImageField(upload_to='goods_images', blank=True, null=True, verbose_name='image')
+    price = models.DecimalField(default = 0.00, max_digits=7, decimal_places=2, verbose_name='price')
+    discount = models.DecimalField(default=0.00, max_digits=7, decimal_places=2, verbose_name="Discount in %")
+    quantity = models.PositiveIntegerField(default=0, verbose_name='quantity')
+    category = models.ForeignKey(to=Categories, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
+
+    class Meta():
+        db_table = 'product'
--- goods/views.py
+++ goods/views.py
@@ -1,8 +1,18 @@
 from django.shortcuts import render
 
+from goods.models import Categories, Products
+
 # Create your views here.
 def catalog(request):
-    return render(request, 'goods/catalog.html')
+
+    categories = Categories.objects.all()
+    products = Products.objects.all()
+
+    context = {
+        'categories': categories,
+        'products': products
+    }
+    return render(request, 'goods/catalog.html', context)
 
 def product(request):
     return render(request, 'goods/product.html')
