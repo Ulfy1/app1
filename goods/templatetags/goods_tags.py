9b5ff9128a581c4e4ecfd6e6806e064033e5b0d8
John Doe <johndoe@email.com>
2025-06-01 17:49:04 +0200

    aa

---
+++ goods/templatetags/goods_tags.py
@@ -0,0 +1,11 @@
+from django import template
+from django.utils.http import urlencode
+from goods.models import Categories
+
+register = template.Library()
+
+@register.simple_tag(takes_context=True)
+def change_params(context, **kwargs):
+    query = context["request"].GET.dict()
+    query.update(kwargs)
+    return urlencode(query)
--- goods/views.py
+++ goods/views.py
@@ -1,9 +1,15 @@
 from django.shortcuts import render
+from django.core.paginator import Paginator
 
 from goods.models import Categories, Products
 
 # Create your views here.
 def catalog(request, category_slug):
+    
+    page = request.GET.get('page', 1)
+    max_price = request.GET.get('max_price')
+    min_price = request.GET.get('min_price')
+
 
     categories = Categories.objects.all()
 
@@ -12,9 +18,22 @@ def catalog(request, category_slug):
     else:
         products = Products.objects.filter(category__slug = category_slug)
 
+    
+
+    if min_price:
+        products = products.filter(price__gte=min_price)
+    if max_price:
+        products = products.filter(price__lte=max_price)
+
+    paginator = Paginator(products, 4)
+    current_page = paginator.page(page)
+
+    products = products.order_by('id')
+
     context = {
         'categories': categories,
-        'products': products
+        'products': current_page,
+        'slug_url': category_slug,
     }
     return render(request, 'goods/catalog.html', context)
 
